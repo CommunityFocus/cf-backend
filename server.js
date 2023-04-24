844f5320256b2faff7a4435c8fa738a468cf93b8
nmpereira <nikhilpereira25@gmail.com>
2023-04-24 04:50:01 -0400

    update server to support basic socket io timer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 const httpServer = createServer(app);
@@ -16,6 +16,28 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("create-something", (msg) => {
+    console.log("message: ", msg, typeof msg, Number(msg), Number(msg) || 100);
+
+    let countTill = Number(msg) || 100;
+
+    // create a timer
+    const timer = setInterval(() => {
+      // emit a message to the client
+      countTill--;
+      socket.emit("timer", countTill);
+      console.log(countTill);
+    }, 1000);
+
+    // stop the timer when countTill is -1
+    setTimeout(() => {
+      clearInterval(timer);
+    }, countTill * 1000 + 1000);
+  });
 });
 
 httpServer.listen(PORT, () => {
