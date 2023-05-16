aa765f801db7a20aba40402a5d6e212347967388
nmpereira <nikhilpereira25@gmail.com>
2023-05-16 00:11:48 -0400

    fix startTimer condition to prevent running if negative input

--- helpers/startTimer.js
+++ helpers/startTimer.js
@@ -1,8 +1,8 @@
 // define the function that will handle the countdown for a room
 function startCountdown({ roomName, durationInSeconds, io, timerStore }) {
   // if there's already a timer instance for this room, clear it
-  if (timerStore[roomName]['timer']) {
-    clearInterval(timerStore[roomName]['timer']);
+  if (timerStore[roomName]["timer"]) {
+    clearInterval(timerStore[roomName]["timer"]);
   }
 
   // emit a message to the room to let everyone know the countdown has started
@@ -10,20 +10,19 @@ function startCountdown({ roomName, durationInSeconds, io, timerStore }) {
 
   // set up the timer instance
   let remainingTime = durationInSeconds;
-  timerStore[roomName]['timer'] = setInterval(() => {
-    // decrement the remaining time
-    remainingTime--;
-
-    // emit the updated remaining time to the room
-    io.to(roomName).emit("timerUpdated", remainingTime);
-
-    console.log({ roomName, remainingTime });
-
+  timerStore[roomName]["timer"] = setInterval(() => {
     // if the timer has reached zero, clear the interval and emit a message to the room
-    if (remainingTime === 0) {
-      clearInterval(timerStore[roomName]['timer']);
+    if (remainingTime <= 0) {
+      clearInterval(timerStore[roomName]["timer"]);
       io.to(roomName).emit("timerEnded");
+    } else {
+      // decrement the remaining time
+      remainingTime--;
+
+      // emit the updated remaining time to the room
+      io.to(roomName).emit("timerUpdated", remainingTime);
     }
+    console.log({ roomName, remainingTime });
   }, 1000);
 }
 
