cd5898cd6e0ad1ee256e4403e43015a7fd60d31a
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-27 10:55:28 -0400

    remove the while loop in timerRequest in favor of a setInterval

--- helpers/timerRequest.js
+++ helpers/timerRequest.js
@@ -1,23 +1,51 @@
-const timerRequest = ({ io, timerStore, roomName, socket }) => {
-  let currentSecondsRemaining = timerStore[roomName].secondsRemaining;
-  let isEventEmitted = false;
-
-  while (
-    timerStore[roomName].secondsRemaining <= currentSecondsRemaining &&
-    !isEventEmitted
-  ) {
-    if (currentSecondsRemaining <= timerStore[roomName].secondsRemaining) {
-      currentSecondsRemaining = timerStore[roomName].secondsRemaining;
-
-      socket.emit("timerResponse", {
-        secondsRemaining: currentSecondsRemaining,
-        isPaused: timerStore[roomName].isPaused,
-      });
-
-      isEventEmitted = true;
-      break;
-    }
+const timerRequest = ({ timerStore, roomName, socket }) => {
+  if (!timerStore[roomName]) {
+    console.error("timerStore[roomName] is undefined | null");
+    return;
   }
+  if (
+    !timerStore[roomName].secondsRemaining ||
+    typeof timerStore[roomName].secondsRemaining !== "number"
+  ) {
+    console.error(
+      "timerStore[roomName].secondsRemaining is not a number, or is undefined | null"
+    );
+    return;
+  }
+
+  if (timerStore[roomName].isPaused) {
+    socket.emit("timerResponse", {
+      secondsRemaining: timerStore[roomName].secondsRemaining,
+      isPaused: timerStore[roomName].isPaused,
+    });
+    return;
+  }
+
+  let currentSecondsRemaining = timerStore[roomName].secondsRemaining;
+
+  // Function to check if secondsRemaining has changed
+  const hasSecondsRemainingChanged = () => {
+    return timerStore[roomName].secondsRemaining !== currentSecondsRemaining;
+  };
+
+  // Emit 'timerResponse' event when secondsRemaining ticks down
+  const emitTimerResponse = () => {
+    currentSecondsRemaining = timerStore[roomName].secondsRemaining;
+    socket.emit("timerResponse", {
+      secondsRemaining: currentSecondsRemaining,
+      isPaused: timerStore[roomName].isPaused,
+    });
+    clearInterval(updateChecker);
+    return;
+  };
+
+  // Check for updates and emit 'timerResponse' every second
+
+  const updateChecker = setInterval(() => {
+    if (hasSecondsRemainingChanged()) {
+      emitTimerResponse();
+    }
+  }, 1); // Check every 1 ms
 };
 
 module.exports = { timerRequest };
