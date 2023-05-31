81e6753414de79a5b0f0ec192f452bab87300df4
riddlew <riddle.w@gmail.com>
2023-05-30 22:36:38 -0400

    Add typescript to eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,25 +7,33 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
   overrides: [
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 'latest',
   },
   plugins: [
+    '@typescript-eslint',
     'unused-imports',
     'prefer-arrow',
     'prettier',
   ],
   rules: {
     'prettier/prettier': ['error'],
+		"@typescript-eslint/no-unused-vars": ["error"],
     "import/prefer-default-export": "warn",
     "import/extensions": ["error", "never"],
     "no-plusplus": "off",
     "radix": ["error", "as-needed"],
     "no-restricted-syntax": "off",
+		"@typescript-eslint/explicit-function-return-type": "warn",
     "unused-imports/no-unused-imports": "error",
     "prefer-arrow/prefer-arrow-functions": [
       "warn", {
